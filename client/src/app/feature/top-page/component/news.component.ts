45c9fd988acd38d6f61d7dbc9c166d27395c085d
OtsukiToru <117517667+pentyann12@users.noreply.github.com>
2024-12-08 14:23:56 +0900

    [update]ニュース取得処理
    ニュースをAPIから取得するよう変更

--- client/src/app/feature/top-page/component/news.component.ts
+++ client/src/app/feature/top-page/component/news.component.ts
@@ -1,36 +1,25 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Inject, OnInit } from '@angular/core';
 import { News } from '../domain/news';
 import { DatePipe, NgFor } from '@angular/common';
+import { TopPageService } from '../service/top-page.service';
 
 @Component({
   selector: 'app-news',
   imports: [DatePipe, NgFor],
   templateUrl: './news.component.html',
-  styleUrl: './news.component.scss'
+  styleUrl: './news.component.scss',
 })
 export class NewsComponent implements OnInit {
   public newsList: Array<News> = []
 
   /** constructor of {@link NewsComponent}. */
-  public constructor() {}
+  public constructor(
+    @Inject(TopPageService) private readonly topPageService: TopPageService
+  ) {}
 
   public ngOnInit(): void {
-    this.newsList = [
-      {
-        date: new Date(2024, 11, 8),
-        category: 'release',
-        description: '新規作成'
-      },
-      {
-        date: new Date(2024, 11, 8),
-        category: 'update',
-        description: 'test'
-      },
-      {
-        date: new Date(2024, 11, 8),
-        category: 'update',
-        description: 'test'
-      },
-    ]
+    this.topPageService.fetchNews().subscribe({
+      next: response => this.newsList = response
+    });
   }
 }
