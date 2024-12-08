037df65dfc9909036330a555c84bd477a7891d6f
OtsukiToru <117517667+pentyann12@users.noreply.github.com>
2024-12-08 13:28:22 +0900

    [update]ニュース表示実装

--- client/src/app/feature/top-page/component/news.component.ts
+++ client/src/app/feature/top-page/component/news.component.ts
@@ -1,11 +1,36 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { News } from '../domain/news';
+import { DatePipe, NgFor } from '@angular/common';
 
 @Component({
   selector: 'app-news',
-  imports: [],
+  imports: [DatePipe, NgFor],
   templateUrl: './news.component.html',
   styleUrl: './news.component.scss'
 })
-export class NewsComponent {
+export class NewsComponent implements OnInit {
+  public newsList: Array<News> = []
 
+  /** constructor of {@link NewsComponent}. */
+  public constructor() {}
+
+  public ngOnInit(): void {
+    this.newsList = [
+      {
+        date: new Date(2024, 11, 8),
+        category: 'release',
+        description: '新規作成'
+      },
+      {
+        date: new Date(2024, 11, 8),
+        category: 'update',
+        description: 'test'
+      },
+      {
+        date: new Date(2024, 11, 8),
+        category: 'update',
+        description: 'test'
+      },
+    ]
+  }
 }
