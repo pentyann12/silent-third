ba4aa874518a5bfd5fdb8dca34cf7d068d6eeb08
OtsukiToru <117517667+pentyann12@users.noreply.github.com>
2024-12-08 14:23:06 +0900

    [feature]API-GET:news生成

--- server/src/app.controller.ts
+++ server/src/app.controller.ts
@@ -1,12 +1,18 @@
 import { Controller, Get } from '@nestjs/common';
 import { AppService } from './app.service';
+import { News } from './domain/news';
 
-@Controller()
+@Controller({})
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
   @Get()
   getHello(): string {
     return this.appService.getHello();
+  }
+
+  @Get('news')
+  public getNews(): Array<News> {
+    return this.appService.getNews();
   }
 }
--- server/src/app.service.ts
+++ server/src/app.service.ts
@@ -1,8 +1,40 @@
 import { Injectable } from '@nestjs/common';
+import { News } from './domain/news';
 
 @Injectable()
 export class AppService {
   getHello(): string {
     return 'Hello World!';
+  }
+
+  /** ニュース配列を返す */
+  public getNews(): Array<News> {
+    return [
+      {
+        date: new Date(2024, 11, 8),
+        category: 'release',
+        description: '新規作成'
+      },
+      {
+        date: new Date(2024, 11, 8),
+        category: 'update',
+        description: 'test1'
+      },
+      {
+        date: new Date(2024, 11, 8),
+        category: 'update',
+        description: 'test2'
+      },
+      {
+        date: new Date(2024, 11, 8),
+        category: 'update',
+        description: 'test4'
+      },
+      {
+        date: new Date(2024, 11, 8),
+        category: 'update',
+        description: 'test5'
+      },
+    ];
   }
 }
