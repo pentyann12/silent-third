491cd70a4e611dcc6faef01fd0f68ff31bfb157e
OtsukiToru <117517667+pentyann12@users.noreply.github.com>
2024-12-08 13:02:13 +0900

    [feature]TopPageComponent生成

---
+++ client/src/app/feature/top-page/top-page.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-top-page',
+  imports: [],
+  templateUrl: './top-page.component.html',
+  styleUrl: './top-page.component.scss'
+})
+export class TopPageComponent {
+
+}
